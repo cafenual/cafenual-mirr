828322bbaabc253564eb6f9aa578b3da977b2528
MIRR820 <tlsalfm820@naver.com>
2021-08-10 22:01:32 +0900

    Implements : login & register API

--- front/src/pages/Login/Login.js
+++ front/src/pages/Login/Login.js
@@ -1,42 +1,77 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
-  const [userid, setUserId] = useState("");
-  const [userpw, setUserPw] = useState("");
+  const [error, setError] = useState();
 
-  const onChangeId = (e) => {
-    setUserId(e.target.value);
-    console.log(e.target.value);
+  const history = useHistory();
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChange = (e) => {
+    console.log(e.target);
+    const nextForm = {
+      ...form, //진짜 form이 아니라 form의 형태를 가져옴 복사를 해옴
+      [e.target.name]: e.target.value, //왼쪽 해당하는 이름을 form에서 가져오기 우측 방금 입력한 값을 form에 씌우기
+    };
+
+    setForm(nextForm); //복사본에 value를 저장해서 setForm을 하면 진짜 form에 value를 덮어씌우기
   };
 
-  const onChangePw = (e) => {
-    setUserPw(e.target.value);
-    console.log(e.target.value);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("실행");
+    console.log(form);
+
+    const body = {
+      email,
+      password,
+    };
+
+    try {
+      await axios.post("http://localhost:4000/api/user/login", body);
+      history.push("/Dashboard");
+    } catch (e) {
+      setError(e.response.data.message);
+    }
   };
+
+  const { email, password } = form;
 
   return (
     <div>
       <body id="login">
-        <form action="homepage.html" name="login" className="login">
+        <form
+          action="homepage.html"
+          name="login"
+          className="login"
+          onSubmit={onSubmit}
+        >
           <legend>로그인</legend>
           <input
-            type="text"
+            type="email"
             placeholder="아이디"
-            name="login_id"
-            onChange={onChangeId}
+            name="email"
+            value={email}
+            onChange={onChange}
           />
           <br />
           <input
             type="password"
             placeholder="비밀번호"
-            name="login_pw"
-            onChange={onChangePw}
+            name="password"
+            onChange={onChange}
           />
           <br />
-          <input type="submit" value="로그인" />
+          <button type="submit">로그인</button>
+          <p>{error}</p>
           <div className="signup">
-            회원이 아니시라면 <a href="signup.html">회원가입</a>을 해주시기
+            회원이 아니시라면 <a href="/signup">회원가입</a>을 해주시기
             바랍니다.
           </div>
         </form>
--- front/src/pages/Signup/Signup.js
+++ front/src/pages/Signup/Signup.js
@@ -1,9 +1,55 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Signup.scss";
+import { useHistory } from "react-router-dom";
 
 const Signup = () => {
   const [userpw, setUserPw] = useState("");
   const [userpwCheck, setUserPwCheck] = useState("");
+
+  const history = useHistory(); //주소를 바꿀 수 있도록 도와주는 Hooks
+
+  const [form, setForm] = useState({
+    email: "",
+    name: "",
+    phoneNumber: "",
+    password: "",
+    passwordCheck: "",
+  });
+
+  const { email, name, phoneNumber, password, passwordCheck } = form; //비구조 할당
+
+  const onChange = (e) => {
+    console.log(e.target);
+    const nextForm = {
+      ...form, //진짜 form이 아니라 form의 형태를 가져옴 복사를 해옴
+      [e.target.name]: e.target.value, //왼쪽 해당하는 이름을 form에서 가져오기 우측 방금 입력한 값을 form에 씌우기
+    };
+
+    setForm(nextForm); //복사본에 value를 저장해서 setForm을 하면 진짜 form에 value를 덮어씌우기
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("실행");
+    console.log(form);
+
+    const body = {
+      email,
+      password,
+      name,
+      phoneNumber,
+    };
+
+    axios
+      .post("http://localhost:4000/api/user/register", body)
+      .then((response) => {
+        console.log(response);
+        if (response.data.success) {
+          history.push("/login");
+        }
+      });
+  };
 
   const onChangepw = (e) => {
     setUserPw(e.target.value);
@@ -41,17 +87,17 @@ const Signup = () => {
             </center>
             <div className="middle">
               <div className="sign-form">
-                <form
-                  className="signup"
-                  action=""
-                  method=""
-                  onSubmit={onSubmitCheck}
-                >
+                <form className="signup" onSubmit={onSubmit}>
                   <p>
                     <strong>성명</strong>
                     <br />
-                    <input type="text" name="lname" placeholder="성" />
-                    <input type="text" name="fname" placeholder="이름" />
+                    <input
+                      type="text"
+                      name="name"
+                      onChange={onChange}
+                      value={name}
+                      placeholder="이름"
+                    />
                   </p>
                   <p>
                     <strong>이메일</strong>
@@ -59,6 +105,8 @@ const Signup = () => {
                     <input
                       type="email"
                       name="email"
+                      onChange={onChange}
+                      value={email}
                       placeholder="아이디로 사용됩니다."
                     />
                     <a>이메일 확인</a>
@@ -69,8 +117,10 @@ const Signup = () => {
                     <input
                       type="password"
                       id="password1"
+                      name="password"
                       placeholder="비밀번호"
-                      onChange={onChangepw}
+                      value={password}
+                      onChange={onChange}
                     />
                   </p>
                   <div className="caution">
@@ -82,8 +132,10 @@ const Signup = () => {
                     <input
                       type="password"
                       id="password2"
+                      name="passwordCheck"
                       placeholder="비밀번호 확인"
-                      onChange={onChangepwCheck}
+                      value={passwordCheck}
+                      onChange={onChange}
                     />
                     <button onclick="check()">비밀번호 확인</button>
                   </p>
@@ -93,12 +145,14 @@ const Signup = () => {
                     <br />
                     <input
                       type="tel"
-                      name="telephone"
+                      name="phoneNumber"
+                      onChange={onChange}
+                      value={phoneNumber}
                       placeholder="전화번호 입력"
                     />
                   </p>
                   <p>
-                    <input type="submit" value="제출" />
+                    <button type="submit">제출</button>
                   </p>
                 </form>
               </div>
